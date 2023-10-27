2df26cce9f18f5a85295e03b9816957592d6830a
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2023-10-27 20:15:37 +0300

    init

---
+++ dist/main.js
@@ -0,0 +1,175 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/Popover.js":
+/*!***************************!*\
+  !*** ./src/js/Popover.js ***!
+  \***************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Popover; }
+/* harmony export */ });
+class Popover {
+  constructor(btn, content) {
+    this.btn = btn;
+    this.popover = content;
+    this.togglePopover = this.togglePopover.bind(this);
+  }
+  init() {
+    this.btn.addEventListener('click', this.togglePopover);
+  }
+  togglePopover(e) {
+    e.preventDefault();
+    const popover = document.querySelector('.popover');
+    if (popover) {
+      popover.remove();
+    } else {
+      document.body.appendChild(this.popover);
+      const {
+        top,
+        left
+      } = this.btn.getBoundingClientRect();
+      const offsetHorizont = (this.popover.offsetWidth - this.btn.offsetWidth) / 2;
+      this.popover.style.left = `${left - offsetHorizont}px`;
+      this.popover.style.bottom = `${top + this.popover.offsetHeight + 14}px`;
+    }
+  }
+  set popover(content) {
+    this._popover = document.createElement('div');
+    const popoverTitle = document.createElement('h3');
+    const popoverText = document.createElement('div');
+    this._popover.classList.add('popover');
+    popoverTitle.classList.add('popover-title');
+    popoverTitle.textContent = content.title;
+    popoverText.classList.add('popover-text');
+    popoverText.textContent = content.message;
+    this._popover.appendChild(popoverTitle);
+    this._popover.appendChild(popoverText);
+  }
+  get popover() {
+    return this._popover;
+  }
+}
+
+/***/ }),
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Popover__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Popover */ "./src/js/Popover.js");
+
+const button = document.querySelector('.popover-btn');
+const content = {};
+content.title = 'Popover title';
+content.message = 'And here\'s some amazing content. It\'s very engaging. Right?';
+const popover = new _Popover__WEBPACK_IMPORTED_MODULE_0__["default"](button, content);
+popover.init();
+
+/***/ }),
+
+/***/ "./src/index.html":
+/*!************************!*\
+  !*** ./src/index.html ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// Module
+var code = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\r\n    <title>Popover</title>\r\n</head>\r\n<body>\r\n    <div class=\"popover-container\">\r\n        <button class=\"popover-btn\">Click to toggle popover</button>\r\n    </div>\r\n</body>\r\n</html>\r\n";
+// Exports
+/* harmony default export */ __webpack_exports__["default"] = (code);
+
+/***/ }),
+
+/***/ "./src/css/style.css":
+/*!***************************!*\
+  !*** ./src/css/style.css ***!
+  \***************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+!function() {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/app */ "./src/js/app.js");
+/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.html */ "./src/index.html");
+/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./css/style.css */ "./src/css/style.css");
+
+
+
+}();
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
